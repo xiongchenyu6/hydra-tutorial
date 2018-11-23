4f6375cf3eb601a4b3e5d2cd5e9398172811c742
Freeman.Xiong <xiongchenyu6@gmail.com>
2018-11-23 20:51:16 +0800

    Update jobset-nixpkgs.nix

--- jobset-nixpkgs.nix
+++ jobset-nixpkgs.nix
@@ -7,7 +7,7 @@ with (import <nixpkgs/pkgs/top-level/release-lib.nix> { inherit supportedSystems
 {
 
   # 'hydraJob' strips all non-essential attributes.
-  hello_world_2 = pkgs.lib.hydraJob pkgs_x86_64_linux.hello;
+  hello_world_2 = pkgs.lib.hydraJob pkgs.x86_64_linux.hello;
 
   # Generate one attribute per supported platform.
   hello_world_3 = pkgs.lib.genAttrs supportedSystems (system: (pkgsFor system).hello);
